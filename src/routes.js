8b7f710659fd8ca2462b1965ff7fd59eb8065e3f
Luciano Liboni <luciano_liboni@hotmail.com>
2020-03-13 20:10:07 -0300

    Adicionando middleware de autenticação

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,18 @@ import SessionController from './app/controllers/SessionController';
 import FunctionsController from './app/controllers/FunctionsController';
 import VolunteersController from './app/controllers/VolunteersController';
 
+import AuthMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
+
+/** Rotas para Voluntários */
+routes.post('/volunteers', VolunteersController.store);
 
 /** Rota para autenticação */
 routes.post('/session', SessionController.store);
+
+/** Rotas que precisam de autenticação */
+routes.use(AuthMiddleware);
 
 /** Rotas para Funções */
 routes.get('/funcoes', FunctionsController.index);
@@ -15,8 +23,6 @@ routes.post('/funcoes', FunctionsController.store);
 routes.put('/funcoes/:id', FunctionsController.update);
 routes.delete('/funcoes/:id', FunctionsController.delete);
 
-/** Rotas para Voluntários */
-routes.post('/volunteers', VolunteersController.store);
 routes.get('/volunteers', VolunteersController.index);
 routes.get('/volunteers/:id', VolunteersController.index);
 
